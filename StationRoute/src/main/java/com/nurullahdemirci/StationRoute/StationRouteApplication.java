934ac7fc31b1e7959bc599c7d65b73c54c917913
Nurullah Demirci <nurullahdemirci061@gmail.com>
2022-01-12 07:00:25 +0300

    add project

---
+++ StationRoute/src/main/java/com/nurullahdemirci/StationRoute/StationRouteApplication.java
@@ -0,0 +1,13 @@
+package com.nurullahdemirci.StationRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationRouteApplication.class, args);
+	}
+
+}
---
+++ StationRoute/src/test/java/com/nurullahdemirci/StationRoute/StationRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurullahdemirci.StationRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
